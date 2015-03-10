59a728f06b81fe2ca485c63d6700a2d38ec35f49
LightGuard <lightguard.jp@gmail.com>
2015-03-10 17:03:16 -0400

    Adding the Book Service (dao)

--- src/main/java/edu/persistence/model/Book.java
+++ src/main/java/edu/persistence/model/Book.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -13,6 +14,7 @@ import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "book", schema = "public", uniqueConstraints = @UniqueConstraint(columnNames = "isbn"))
+@NamedQuery(name = "bookByIsbn", query = "SELECT b from Book b WHERE b.isbn = :isbn")
 public class Book implements java.io.Serializable {
 
 	@Id
---
+++ src/main/java/edu/persistence/service/BookDao.java
@@ -0,0 +1,64 @@
+package edu.persistence.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import edu.persistence.model.Book;
+
+/**
+ *  DAO for Book
+ */
+@Stateless
+public class BookDao
+{
+   @PersistenceContext(unitName = "jpa-demo")
+   private EntityManager em;
+
+   public void create(Book entity)
+   {
+      em.persist(entity);
+   }
+
+   public void deleteById(Long id)
+   {
+      Book entity = em.find(Book.class, id);
+      if (entity != null)
+      {
+         em.remove(entity);
+      }
+   }
+
+   public Book findById(Long id)
+   {
+      return em.find(Book.class, id);
+   }
+
+   public Book findByIsbn(String isbn) {
+      TypedQuery<Book> findByIsbn = em.createNamedQuery("bookByIsbn", Book.class);
+      findByIsbn.setParameter("isbn", isbn);
+
+      return findByIsbn.getSingleResult();
+   }
+
+   public Book update(Book entity)
+   {
+      return em.merge(entity);
+   }
+
+   public List<Book> listAll(Integer startPosition, Integer maxResult)
+   {
+      TypedQuery<Book> findAllQuery = em.createQuery("SELECT DISTINCT b FROM Book b ORDER BY b.id", Book.class);
+      if (startPosition != null)
+      {
+         findAllQuery.setFirstResult(startPosition);
+      }
+      if (maxResult != null)
+      {
+         findAllQuery.setMaxResults(maxResult);
+      }
+      return findAllQuery.getResultList();
+   }
+}
