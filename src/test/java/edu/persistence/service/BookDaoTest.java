a3d012d0fa222a1dd18ac34ab6074d18b2f033df
LightGuard <lightguard.jp@gmail.com>
2015-03-11 16:38:03 -0400

    Adding arquillian tests
    VERY basic tests, but things are setup

---
+++ src/test/java/edu/persistence/service/BookDaoTest.java
@@ -0,0 +1,37 @@
+package edu.persistence.service;
+
+import edu.persistence.service.BookDao;
+import javax.inject.Inject;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+@RunWith(Arquillian.class)
+public class BookDaoTest
+{
+
+   @Inject
+   private BookDao bookdao;
+
+   @Deployment
+   public static JavaArchive createDeployment()
+   {
+      return ShrinkWrap.create(JavaArchive.class)
+            .addClass(BookDao.class)
+            .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+   }
+
+   @Test
+   public void should_be_deployed()
+   {
+      Assert.assertNotNull(bookdao);
+   }
+}
